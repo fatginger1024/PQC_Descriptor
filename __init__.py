32f9cedce339664b9a93ff50956cce06651fb3df
Qing Zhou <zhouqing@qudoor.cn>
2021-11-08 16:15:22 +0800

    add update to test

---
+++ __init__.py
@@ -0,0 +1,6 @@
+from interface import Interface
+from multiprocess import Multiprocess
+from run import Simulation
+from analyser import Analyser
+from descriptors.expressibility import Expressibility
+from descriptors.entanglement import Entanglement_Capability
