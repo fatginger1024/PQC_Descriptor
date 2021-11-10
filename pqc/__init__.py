8c5939a349e65dc655541f0b7121532bf417b87b
Qing Zhou <zhouqing@qudoor.cn>
2021-11-10 16:58:29 +0800

    update init

--- pqc/__init__.py
+++ pqc/__init__.py
@@ -6,5 +6,6 @@ from pqc.multiprocess import Multiprocess
 from pqc.run import Simulation
 from pqc.analyser import Analyser
 from pqc.analyse import analyse
+from pqc.featuremap import FeatureMap
 #from pqc.descriptors.expressibility import Expressibility
 #from pqc.descriptors.entanglement import Entanglement_Capability
